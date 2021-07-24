e99fdfc72590260c0928e561105a147516b770f5
Metaphysical1 <87896576+Metaphysical1@users.noreply.github.com>
2021-07-24 13:09:58 +0200

    Random naming, to avoid VAC ?

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-
+rm -rf Release
 mkdir Release && cd Release && cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=gcc-10 -D CMAKE_CXX_COMPILER=g++-10 ..
 
 make -j $(nproc --all)
 
-mv libOsiris.so ..
+#get random string 
+random_name=`tr -dc A-Za-z0-9 </dev/urandom | head -c 13 `
+
+mv libOsiris.so $random_name
+cp ../run.sh ./
+sed -i "s/libOsiris.so/$random_name/" run.sh
